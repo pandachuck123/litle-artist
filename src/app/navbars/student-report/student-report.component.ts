783fd2ed8dbb5c71988a83ecc8b697d85f7169b2
SharmilaDeviS <sharmiladevi5795@gmail.com>
2020-12-31 00:04:51 +0530

    commit

--- src/app/navbars/student-report/student-report.component.ts
+++ src/app/navbars/student-report/student-report.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-student-report',
@@ -6,10 +7,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-report.component.scss']
 })
 export class StudentReportComponent implements OnInit {
+public form:FormGroup;
+public CityValid:any
+public image:any
+public uploadPath:any
+public studentLists:any
+public config:any
+public imageSrc:any
 
-  constructor() { }
+  constructor( public fb: FormBuilder) {
+    this.CityValid = false;
+    this.image = [];
+    this.uploadPath = [];
+
+    this.form = this.fb.group({
+      'studentName':'',
+      'file':'',
+      'fileSource':'',
+    });
+
+    this.studentLists=[
+      '1','2'
+    ]
+
+    this.config = {
+      displayKey: "city", //if objects array passed which key to be displayed defaults to description
+      search: true,
+      limitTo: 10,
+    };
+    this.imageSrc='';
+  }
 
   ngOnInit(): void {
+  }
+  onFileChange(event) {
+    const reader = new FileReader();
+
+    if (event.target.files && event.target.files.length) {
+      const [file] = event.target.files;
+      reader.readAsDataURL(file);
+
+      reader.onload = () => {
+
+        this.imageSrc = reader.result as string;
+
+        this.form.patchValue({
+          fileSource: reader.result
+        });
+
+      };
+
+    }
+  }
+  onNoClick(){
+   this.imageSrc='';
+    this.form.controls['file'].patchValue('')
+    this.form.controls['fileSource'].patchValue('')
   }
 
 
