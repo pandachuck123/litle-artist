d341c7d58b08d3053977000d1ed597b5d188690d
SharmilaDeviS <sharmiladevi5795@gmail.com>
2021-01-05 12:40:44 +0530

    commit

--- src/app/navbars/teacher-report/teacher-report.component.ts
+++ src/app/navbars/teacher-report/teacher-report.component.ts
@@ -9,11 +9,83 @@ import {TeacherReportsService} from "../../services/teacher-reports.service";
 })
 export class TeacherReportComponent implements OnInit {
   public report: any;
+  public reportList: any;
 
-  constructor(public SpinnerService: NgxSpinnerService, public teachReport: TeacherReportsService) { }
+  constructor(public SpinnerService: NgxSpinnerService, public teachReport: TeacherReportsService) {
+    this.SpinnerService.show();
+    this.reportList=[];
+    this.reportList.push(
+      {
+      StudentRegNo:'001',
+      Name: 'raja',
+      Std: '3',
+      Marks: '39',
+      Age: '8',
+      Address: 'kk Nagar',
+      City: 'chennai',
+      ContactNum: '9788667788',
+    },
+      {
+        StudentRegNo:'002',
+        Name: 'sharmi',
+        Std: '5',
+        Marks: '79',
+        Age: '10',
+        Address: 'vadapalani',
+        City: 'chennai',
+        ContactNum: '9788667784',
+      }, {
+        StudentRegNo:'003',
+        Name: 'keerthi',
+        Std: '6',
+        Marks: '79',
+        Age: '11',
+        Address: 't Nagar',
+        City: 'chennai',
+        ContactNum: '9788667756',
+      }, {
+        StudentRegNo:'004',
+        Name: 'venkat',
+        Std: '4',
+        Marks: '94',
+        Age: '9',
+        Address: 'kk Nagar',
+        City: 'chennai',
+        ContactNum: '9788664488',
+      }, {
+        StudentRegNo:'005',
+        Name: 'sugha',
+        Std: '3',
+        Marks: '53',
+        Age: '8',
+        Address: ' chrompet',
+        City: 'chennai',
+        ContactNum: '9788667788',
+      },
+    );
+
+    console.log(this.reportList,'reportList....')
+    // this.report=JSON.parse( this.reportList);
+    this.report=( this.reportList);
+    console.log(this.report,'report...')
+    this.SpinnerService.hide();
+
+    // this.report.push({
+    //   StudentRegNo:1,
+    //   Name: 'ABC',
+    //   Std: '3',
+    //   Marks: '39',
+    //   Age: '10',
+    //   Address: 'kk Nagar',
+    //   City: 'chennai',
+    //   ContactNum: '9788667788',
+    // });
+
+
+  }
 
   ngOnInit(): void {
-    this.teacherDetails();
+    // this.teacherDetails();
   }
   teacherDetails() {
     this.SpinnerService.show();
